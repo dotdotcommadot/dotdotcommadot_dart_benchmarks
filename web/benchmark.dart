9dbe33134365b4aa83e3629df821f84ee937a0d6
dotdotcommadot <hans@dotdotcommadot.com>
2015-06-29 17:32:32 +0200

    Adds Parts

--- web/benchmark.dart
+++ web/benchmark.dart
@@ -12,25 +12,35 @@ part 'package:benchmarks/impl/loops/foreach_clazz.dart';
 part 'package:benchmarks/impl/operator_overloading/no_operator_overloading.dart';
 part 'package:benchmarks/impl/operator_overloading/operator_overloading.dart';
 
+part 'package:benchmarks/impl/strings/string_concatenation_adjacent_literal.dart';
 part 'package:benchmarks/impl/strings/string_concatenation_interpolation.dart';
 part 'package:benchmarks/impl/strings/string_concatenation_plus_operator.dart';
+part 'package:benchmarks/impl/strings/string_concatenation_plus_operator_literal.dart';
+part 'package:benchmarks/impl/strings/string_concatenation_stringbuffer.dart';
 
 part 'package:benchmarks/impl/typing/strong_typing.dart';
 part 'package:benchmarks/impl/typing/weak_typing.dart';
 
 main()
 {
+	// Loops
 	new ForeachClazz().report();
 	new ForeachInt().report();
 	new ForLoopClazz().report();
 	new ForLoopInt().report();
 	
+	// Operator Overloading
 	new NoOperatorOverloading().report();
 	new OperatorOverloading().report();
 	
+	// Strings
+	new StringConcatenationAdjacentLiteral().report();
 	new StringConcatenationInterpolation().report();
 	new StringConcatenationPlusOperator().report();
+	new StringConcatenationPlusOperatorLiteral().report();
+	new StringConcatenationStringBuffer().report();
 	
+	// Typing
 	new StrongTyping().report();
 	new WeakTyping().report();
 }
