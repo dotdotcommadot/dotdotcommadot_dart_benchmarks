8034b4a46fd26a74dfb4bb6b92d6aaccf496eb47
dotdotcommadot <hans@dotdotcommadot.com>
2015-06-30 17:20:05 +0200

    Prevents eliminated JS code

--- lib/impl/strings/string_concatenation_plus_operator.dart
+++ lib/impl/strings/string_concatenation_plus_operator.dart
@@ -5,21 +5,25 @@ class StringConcatenationPlusOperator extends BenchmarkBase
 	static const int ITERATIONS 	= 100000;
 	final String hello 						= 'Hello';
 	final String world 						= 'World';
+	String helloWorld;
 	
 	StringConcatenationPlusOperator() : super("StringConcatenationPlusOperator");
-	
+
+  @override
 	void setup() 
   { 
   }
-	
+
+  @override
 	void run() 
   {
     for (int i = 0; i < ITERATIONS; i++)
     {
-    	var e = '...' + hello + ' ' + world + '...';
+    	helloWorld = '...' + hello + ' ' + world + '...';
     }
   }
 
+  @override
   void teardown() 
   { 
   }
